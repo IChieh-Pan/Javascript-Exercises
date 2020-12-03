1f571a013ab8f1a4e06575d7d0c670ff399efb21
ellenpan <ellenpan@ipad0414.localdomain>
2020-12-03 12:25:50 +0100

    code fixed

--- JS/table_new.js
+++ JS/table_new.js
@@ -1,85 +1,83 @@
 
-function fetchData() {
-  fetch("https://usc.data.socrata.com/resource/4a97-v5tx.json")
-    .then((response) => response.json())
-    .then((data) => {
-      console.log("fetch", data);
-    });
-  // console.log("data :>> ", fetchData);
-  fetchData();
+//Create table from dataset
+
+function createTable(data) {
+  // console.log(createTable(data));
+  let body = document.getElementById("property-table");
+  body.innerHTML = "";
+  let table = document.createElement("table");
+  let tBody = document.createElement("tBody");
+  let th;
+  let td;
+  let thead = document.createElement("thead");
+  let tr = document.createElement("tr");
+  th = document.createElement("th");
+  th.innerHTML = "Neighborhood";
+  tr.appendChild(th);
+
+  th = document.createElement("th");
+  th.innerHTML = "Year";
+  tr.appendChild(th);
+
+  th = document.createElement("th");
+  th.innerHTML = "Amount";
+  tr.appendChild(th);
+
+  thead.appendChild(tr);
+  table.appendChild(thead);
+
+ data.forEach((item) => {
+    tr = document.createElement("tr");
+    td1 = document.createElement("td");
+    td1.innerHTML = item.neighborhood;
+    tr.appendChild(td1);
+
+    td2 = document.createElement("td");
+    td2.innerHTML = item.year;
+    tr.appendChild(td2);
+
+    td3 = document.createElement("td");
+    td3.innerHTML = item.amount;
+    tr.appendChild(td3);
+
+    tBody.appendChild(tr);
+  });
+  table.appendChild(tBody);
+  body.appendChild(table);
+}
+
+const fetchData = async () => {
+  
+  let response  = await fetch("https://usc.data.socrata.com/resource/4a97-v5tx.json")
+  let data = await response.json();
+  return data;
 };
 
 
+function tableDataFilter(data) {
+  let result = [];
+  data.forEach((item) => {
+    if (item.year < 2011) {
+      result.push(item);
+    }
+    console.log(result);
+  });
+  createTable(result);
+}
 
-  function tableDataFilter() {
-    let result = [];
-    dataset.forEach((item) => {
-      if (item.year < 2011) {
-        result.push(item);
-      }
-      console.log(result);
-    });
-    createTable(result);
-  }
-
-  //Create table from dataset
-  function createTable(data) {
-    let body = document.getElementById("property-table");
-    body.innerHTML = "";
-    let table = document.createElement("table");
-    let tBody = document.createElement("tBody");
-    let th;
-    let td;
-    let thead = document.createElement("thead");
-    let tr = document.createElement("tr");
-    th = document.createElement("th");
-    th.innerHTML = "Neighborhood";
-    tr.appendChild(th);
-
-    th = document.createElement("th");
-    th.innerHTML = "Year";
-    tr.appendChild(th);
-
-    th = document.createElement("th");
-    th.innerHTML = "Amount";
-    tr.appendChild(th);
-
-    thead.appendChild(tr);
-    table.appendChild(thead);
-
-    dataset.forEach((item) => {
-      tr = document.createElement("tr");
-      td1 = document.createElement("td");
-      td1.innerHTML = item.neighborhood;
-      tr.appendChild(td1);
-
-      td2 = document.createElement("td");
-      td2.innerHTML = item.year;
-      tr.appendChild(td2);
-
-      td3 = document.createElement("td");
-      td3.innerHTML = item.amount;
-      tr.appendChild(td3);
-
-      tBody.appendChild(tr);
-    });
-    table.appendChild(tBody);
-    body.appendChild(table);
-  }
-
-  // createTable(dataset);
-
-  //Get unique values from attributes in an array
-
+//Get unique values from attributes in an array
+const unique = (data) => {
   const yearMap = new Set(data.map((data) => data.year));
   const yearCount = [...yearMap];
   console.log(yearCount);
 
   const ngrhoodCount = [...new Set(data.map((data) => data.neighborhood))];
-  console.log(ngrhoodCount);
+  // console.log(ngrhoodCount);
+  return yearCount;
+};
 
-
-  //Populate dropdown list from unique value array
+//Populate dropdown list from unique value array
+const dropdown = (yearCount) => {
   yearCount.forEach(function (item) {
     function countYear() {
       const dropdown = document.getElementById("filterBtn");
@@ -90,62 +88,46 @@ function fetchData() {
     }
     countYear();
   });
+};
 
-  //control select tag with array attribute value
-  function filter() {
-    //   let result = [];
-    let q = document.getElementById("filterBtn").value;
-    //   console.log(q);
-    if (q === "default") {
-      createTable(data);
-    } else {
-      const filterArray = data.filter((item) => {
-        return item.year === q && item.neighborhood === "Long Beach";
-      });
+//control select tag with array attribute value
+function filter(data) {
+  //   let result = [];
+  let q = document.getElementById("filterBtn").value;
+  //   console.log(q);
+  if (q === "default") {
+    createTable(data);
+  } else {
+    const filterArray = data.filter((item) => {
+      return item.year === q && item.neighborhood === "Long Beach";
+    });
 
-      /*     dataset.forEach((item) => {
+        data.forEach((item) => {
         if (item.year === q) {
           result.push(item);
         }
-        console.log(
-      }); */
+        // console.log(
+      });
 
-      createTable(filterArray);
-    }
+    createTable(filterArray);
   }
+}
 
-  /*   for (var x = 0; x < dataset.length; x++) {
-           if (dataset[x].year === q) {
-              result.push(dataset[x]);
-          } */
 
-  //DOM control of select tag "onChange" event listener
-  function showSelected() {
-    filter();
+//DOM control of select tag "onChange" event listener
+function showSelected() {
+  filter(data);
+}
+//Checkbox
+function showData(data) {
+  if (document.getElementById("showBefore2012").checked) {
+    tableDataFilter(data);
+  } else {
+    fetchData(data);
   }
-
-  function showData() {
-    if (document.getElementById("showBefore2012").checked) {
-      tableDataFilter();
-    } else {
-      fetchData();
-    }
-  }
+}
 
 
-//
-/* function filter() {
-    let result = [];
-    let q = document.getElementById("filterBtn").value;
-
-    const callback = (item, index) => {
-        if (document.getElementById("filterBtn").value === "default")
-            return;
-        console.log(callback);
-
-    dataset.forEach(callback)
-    }
-    filter() */
 
 /* for (var x = 0; x < dataset.length; x++) {
         if (dataset[x].year != "undefined" && dataset[x].year === q) {
@@ -157,6 +139,15 @@ function fetchData() {
             }
         } */
 
+async function controller() {
+  const data = await fetchData();
+  // console.log(data);
+  createTable(data);
+  unique(data);
+  tableDataFilter(data);
+  filter(data);
+  showData(data);
+  showSelected()
 
-
-
+}
+controller();
