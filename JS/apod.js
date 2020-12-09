095a46fae71b21b33f6244369b7cf4a4ac9b3d9e
ellenpan <ellenpan@ipad0414.localdomain>
2020-12-09 17:09:27 +0100

    New pages added

---
+++ JS/apod.js
@@ -0,0 +1,122 @@
+const getData = () => {
+  fetch("https://apodapi.herokuapp.com/api/?count=200")
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      console.log("data:", data);
+      /* createTable(data);
+      setEventListeners(data); */
+    });
+};
+
+const fetchData = async () => {
+  const response = await fetch("https://apodapi.herokuapp.com/api/?count=200");
+  const data = await response.json();
+  return data;
+};
+
+function createTable(data) {
+  let body = document.getElementById("property-table");
+  body.innerHTML = "";
+  let table = document.createElement("table");
+  let tBody = document.createElement("tbody");
+  let th;
+  let td;
+  let thead = document.createElement("thead");
+  let tr = document.createElement("tr");
+  th = document.createElement("th");
+  th.innerHTML = "title";
+  tr.appendChild(th);
+
+  th = document.createElement("th");
+  th.innerHTML = "copyright";
+  tr.appendChild(th);
+
+  th = document.createElement("th");
+  th.innerHTML = "date";
+  tr.appendChild(th);
+
+  thead.appendChild(tr);
+  table.appendChild(thead);
+
+  data.forEach((item) => {
+    tr = document.createElement("tr");
+    td1 = document.createElement("td");
+    td1.innerHTML = item.title;
+    tr.appendChild(td1);
+
+    td2 = document.createElement("td");
+    td2.innerHTML = item.copyright;
+    tr.appendChild(td2);
+
+    td3 = document.createElement("td");
+    td3.innerHTML = item.date;
+    tr.appendChild(td3);
+
+    tBody.appendChild(tr);
+  });
+  table.appendChild(tBody);
+  body.appendChild(table);
+}
+
+/* function createSort(data) {
+  data.sort(function (a, b) {
+    let nameA = a.title.toLowerCase();
+    let nameB = b.title.toLowerCase();
+    if (nameA < nameB) return 1;
+    if (nameA > nameB) return -1;
+    return 0;
+  })
+  } */
+
+/* const sort = document.getElementById("sortCopyright");
+console.log("sort",sort);
+sort.addEventListener('change', function(event){
+  console.log(event.target.checked);
+  if (event.target.checked) {
+    createSort(data);
+    createTable(data);
+  } else {
+    // createTable(data);
+  }
+}) */
+
+const eventListener = (list) => {
+  document.querySelector("#datePicker").addEventListener("change", (event) => {
+    const datePickerValue = document.querySelector("#datePicker").value;
+    console.log("datePickerValue", datePickerValue);
+    const filterDate = filterByDate(list, datePickerValue);
+    console.log("filterDate :>> ", filterDate);
+    createTable(filterDate);
+  });
+};
+
+const filterByDate = (array, date) => {
+  const filteredList = array.filter((item) => {
+    // console.log(date);
+    // console.log("new Date :>>", new Date(date));
+    console.log("item.date", new Date(item.date).toDateString());
+    return (
+      new Date(item.date).setHours(0, 0, 0, 0) ===
+      new Date(date).setHours(0, 0, 0, 0)
+    );
+  });
+  return filteredList;
+};
+
+/*  const filteredListByDate = filterByDate(item);
+ console.log("filteredListByDate:", filteredListByDate);
+ createTable(filteredListByDate);
+ */
+
+const controller = async () => {
+  const list = await fetchData();
+  console.log("list :>> ", list);
+  createTable(list);
+  eventListener(list);
+  filterByDate(list);
+
+  // createSort(data)
+};
+controller();
--- JS/table_new2.js
+++ JS/table_new2.js
@@ -1,4 +1,3 @@
-
 const fetchData = async () => {
   const response = await fetch(
     "https://usc.data.socrata.com/resource/4a97-v5tx.json"
@@ -7,62 +6,64 @@ const fetchData = async () => {
   return data;
 };
 
-
-
 function createTable(data) {
-  let body = document.getElementById("property-table");
-  body.innerHTML = "";
-  let table = document.createElement("table");
-  let tBody = document.createElement("tBody");
-  let th;
-  let td;
-  let thead = document.createElement("thead");
-  let tr = document.createElement("tr");
-  th = document.createElement("th");
-  th.innerHTML = "Neighborhood";
-  tr.appendChild(th);
+  
+    let table = document.getElementById("property-table");
+    table.innerHTML = "";
+    let tBody = document.createElement("tBody");
+    let th;
+    let td;
+    let thead = document.createElement("thead");
+    document.getElementsByClassName("thead").className = "thead-dark";
+    let tr = document.createElement("tr");
+    th = document.createElement("th");
+    th.innerHTML = "Neighborhood";
+    tr.appendChild(th);
 
-  th = document.createElement("th");
-  th.innerHTML = "Year";
-  tr.appendChild(th);
+    th = document.createElement("th");
+    th.innerHTML = "Year";
+    tr.appendChild(th);
 
-  th = document.createElement("th");
-  th.innerHTML = "Amount";
-  tr.appendChild(th);
+    th = document.createElement("th");
+    th.innerHTML = "Amount";
+    tr.appendChild(th);
 
-  thead.appendChild(tr);
-  table.appendChild(thead);
+    thead.appendChild(tr);
+    table.appendChild(thead);
 
- data.forEach((item) => {
-    tr = document.createElement("tr");
-    td1 = document.createElement("td");
-    td1.innerHTML = item.neighborhood;
-    tr.appendChild(td1);
+  data.forEach((item) => {
+    if (yearValue(item) && amountValue(item)) {
+      tr = document.createElement("tr");
+      td1 = document.createElement("td");
+      td1.innerHTML = item.neighborhood;
+      tr.appendChild(td1);
 
-    td2 = document.createElement("td");
-    td2.innerHTML = item.year;
-    tr.appendChild(td2);
+      td2 = document.createElement("td");
+      td2.innerHTML = item.year;
+      tr.appendChild(td2);
 
-    td3 = document.createElement("td");
-    td3.innerHTML = item.amount;
-    tr.appendChild(td3);
+      td3 = document.createElement("td");
+      td3.innerHTML = item.amount;
+      tr.appendChild(td3);
 
-    tBody.appendChild(tr);
-  });
-  table.appendChild(tBody);
-  body.appendChild(table);
+      tBody.appendChild(tr);
+
+      }
+    });
+    table.appendChild(tBody);
+  
 }
 
+// createTable();
 
 //Get unique values from attributes in an array
-function unique(data)  {
+function unique(data) {
   const yearMap = new Set(data.map((data) => data.year));
   console.log(yearMap);
   const yearCount = [...yearMap];
   console.log(yearCount);
   return yearCount;
-};
-
+}
 
 //Populate dropdown list from unique value array
 const dropdown = (yearCount) => {
@@ -78,17 +79,14 @@ const dropdown = (yearCount) => {
   });
 };
 
-
-
 //control select tag with array attribute value (using .filter() function)
 function filter(data) {
-  let result = [];
   let q = document.getElementById("filterBtn").value;
   if (q === "default") {
     createTable(data);
   } else {
     const filterArray = data.filter((item) => {
-       return  q === item.year;
+      return q === item.year;
     });
     createTable(filterArray);
   }
@@ -98,9 +96,11 @@ const setEventListeners = (data) => {
   const selectBox = document
     .getElementById("filterBtn")
     .addEventListener("change", (event) => {
-      filter(data);
+      // filter(data);
+       createTable(data);
+
     });
-  
+
   const checkbox = document.getElementById("showBefore2012");
   checkbox.addEventListener("change", (e) => {
     if (e.target.checked) {
@@ -109,12 +109,85 @@ const setEventListeners = (data) => {
       createTable(data);
     }
   });
+
+
+  const checkbox2 = document.getElementById("sortAscending");
+  // console.log("sortcheckbox :>> ", checkbox2);
+  checkbox2.addEventListener("change", (event) => {
+    if (event.target.checked) {
+      createSort(data);
+      // createTable(data);
+    } else {
+      console.log(event.target);
+      createTable(data);
+    }
+  });
+
+  const checkbox3 = document.getElementById("filterAmount");
+  checkbox3.addEventListener("change", (event) => {
+     createTable(data);
+    
+    // if (event.target.checked) {
+    //   // console.log('x',filterAmount(data));
+    //   const newList = filterAmount(data, 1000);
+    //   createTable(newList);
+    // } else {
+    //   createTable(data);
+    // }
+  });
+};
+
+  function createSort(data) {
+    const sorted = data;
+    sorted.sort(function (a, b) {
+      let nameA = a.neighborhood.toLowerCase();
+      let nameB = b.neighborhood.toLowerCase();
+      if (nameA < nameB) return 1;
+      if (nameA > nameB) return -1;
+      return 0;
+    });
+    createTable(sorted);
+    // console.log("object :>> ", emptySort.push(sort));
+  }
+
+
+
+const yearValue = (item) => {
+  const input = document.getElementById("filterBtn").value;
+  return item.year === input || input === "default" ? true : false;
+};
+
+function amountValue(item) {
+  if (document.getElementById("filterAmount").checked) {
+    if (item.amount > 1000) {
+      return true;
+    }
+    else return false
+  } else {
+    return true 
+  }
+}
+/* 
+  const combine = (array) => {
+    const input = document.getElementById("filterBtn").value;
+    const checkValue = document.getElementById("filterAmount").checked
+    const filteredList = array.filter(item => {
+  let 
+})
+ */
+    
+  
+
+const filterAmount = (data, newAmount) => {
+  const amountFilter = data.filter((item) => item.amount > newAmount);
+  console.log("amount :>> ", amountFilter);
+  return amountFilter;
 };
 
 function tableDataFilter(data) {
   let result = [];
   data.forEach((item) => {
-    if (item.year < 2011) {
+    if (item.year < 2016) {
       result.push(item);
     }
     // console.log(result);
@@ -122,9 +195,10 @@ function tableDataFilter(data) {
   createTable(result);
 }
 
-
-
-
+const show2016 = (item) => {
+  const show2016Value = document.getElementById("showBefore2012").value;
+  return show2016Value;
+};
 /* function showData(data) {
   if (checkbox.checked) {
     tableDataFilter(data);
@@ -133,20 +207,67 @@ function tableDataFilter(data) {
   }
 } */
 
-
-
 //controller
 const controller = async () => {
   const data = await fetchData();
-  // console.log(data);
+  console.log("list :>> ", data);
   createTable(data);
   const uniqueDates = unique(data);
-  dropdown(uniqueDates)
+  dropdown(uniqueDates);
+  yearValue(data);
   tableDataFilter(data);
   filter(data);
   setEventListeners(data);
-
-}
+};
 controller();
 
+// Bootstrap responsive table
+$(document).ready(function () {
+  // inspired by http://jsfiddle.net/arunpjohny/564Lxosz/1/
+  $(".table-responsive-stack").each(function (i) {
+    var id = $(this).attr("id");
+    //alert(id);
+    $(this)
+      .find("th")
+      .each(function (i) {
+        $("#" + id + " td:nth-child(" + (i + 1) + ")").prepend(
+          '<span class="table-responsive-stack-thead">' +
+            $(this).text() +
+            ":</span> "
+        );
+        $(".table-responsive-stack-thead").hide();
+      });
+  });
 
+  $(".table-responsive-stack").each(function () {
+    var thCount = $(this).find("th").length;
+    var rowGrow = 100 / thCount + "%";
+    //console.log(rowGrow);
+    $(this).find("th, td").css("flex-basis", rowGrow);
+  });
+
+  function flexTable() {
+    if ($(window).width() < 768) {
+      $(".table-responsive-stack").each(function (i) {
+        $(this).find(".table-responsive-stack-thead").show();
+        $(this).find("thead").hide();
+      });
+
+      // window is less than 768px
+    } else {
+      $(".table-responsive-stack").each(function (i) {
+        $(this).find(".table-responsive-stack-thead").hide();
+        $(this).find("thead").show();
+      });
+    }
+    // flextable
+  }
+
+  flexTable();
+
+  window.onresize = function (event) {
+    flexTable();
+  };
+
+  // document ready
+});
