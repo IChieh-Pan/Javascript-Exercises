2802b494ba2e677592f19a18de7db462f0f3ce89
IChieh-Pan <ellenpan7174@gmail.com>
2020-12-14 15:53:54 +0100

    Change date filter to only filter month and year

--- JS/apod.js
+++ JS/apod.js
@@ -1,5 +1,5 @@
 const getData = () => {
-  fetch("https://apodapi.herokuapp.com/api/?count=200")
+  fetch("https://apodapi.herokuapp.com/api/?count=100")
     .then((response) => {
       return response.json();
     })
@@ -11,7 +11,7 @@ const getData = () => {
 };
 
 const fetchData = async () => {
-  const response = await fetch("https://apodapi.herokuapp.com/api/?count=200");
+  const response = await fetch("https://apodapi.herokuapp.com/api/?count=100");
   const data = await response.json();
   return data;
 };
@@ -23,25 +23,34 @@ function createTable(data) {
   let tBody = document.createElement("tbody");
   let th;
   let td;
+  let rowAll = document.createElement("div");
+  rowAll.classList.add("row");
   let thead = document.createElement("thead");
   thead.classList.add("thead-dark");
   let tr = document.createElement("tr");
+  let row = document.createElement("div");
+  row.appendChild(tr);
+  row.classList.add("row");
   th = document.createElement("th");
+  th.classList.add("col-4");
   th.classList.add("sticky-top");
   th.innerHTML = "Title";
   tr.appendChild(th);
 
   th = document.createElement("th");
+  th.classList.add("col-4");
   th.classList.add("sticky-top");
   th.innerHTML = "Copyright";
   tr.appendChild(th);
 
   th = document.createElement("th");
+  th.classList.add("col-4");
   th.classList.add("sticky-top");
   th.innerHTML = "Date";
   tr.appendChild(th);
 
   thead.appendChild(tr);
+  rowAll.appendChild(thead);
   table.appendChild(thead);
 
   data.forEach((item) => {
@@ -51,10 +60,12 @@ function createTable(data) {
     tr.appendChild(td1);
 
     td2 = document.createElement("td");
+
     td2.innerHTML = item.copyright;
     tr.appendChild(td2);
 
     td3 = document.createElement("td");
+
     td3.innerHTML = item.date;
     tr.appendChild(td3);
 
@@ -87,27 +98,61 @@ sort.addEventListener('change', function(event){
 }) */
 
 const eventListener = (list) => {
-  document.querySelector("#datePicker").addEventListener("change", (event) => {
-    const datePickerValue = document.querySelector("#datePicker").value;
+  document.querySelector("#fromDate").addEventListener("change", (event) => {
+    let dateFormat = "yyyyMM";
+    const datePickerValue = document.querySelector("#fromDate").value;
     console.log("datePickerValue", datePickerValue);
-    const filterDate = filterByDate(list, datePickerValue);
-    console.log("filterDate :>> ", filterDate);
-    createTable(filterDate);
+    /* const x = datePickerValue.toLocaleDateString("en", {
+      month: "numeric", year: "numeric"
+    })  */
+    const filterTableDate = tableDate(list, datePickerValue);
+    console.log("filterTableDate :>> ", filterTableDate);
+    createTable(filterTableDate);
   });
 };
 
-const filterByDate = (array, date) => {
+const tableDate = (array, dateX) => {
+  const datepicker = new Date(dateX)
+  const monthDatePicker = datepicker.getMonth();
+  const yearDatePicker = datepicker.getFullYear();
   const filteredList = array.filter((item) => {
-    // console.log(date);
-    // console.log("new Date :>>", new Date(date));
-    console.log("item.date", new Date(item.date).toDateString());
-    return (
-      new Date(item.date).setHours(0, 0, 0, 0) ===
-      new Date(date).setHours(0, 0, 0, 0)
-    );
+    let today = new Date(item.date).toLocaleDateString("en", {
+      month: "numeric",
+      year: "numeric",
+    });
+    console.log("today :>> ", today);
+
+    const monthItem = new Date(item.date).getMonth();
+    const yearItem= new Date(item.date).getFullYear();
+    console.log(yearDatePicker);
+    /*     let year = today.getFullYear();
+    let mon = today.getMonth();
+    year && mon ? true : false  */
+
+    /*     console.log("year",year);
+    console.log('mon :>> ', mon);
+    let monthYear = `${year}-${mon}`; 
+    console.log("x",monthYear); */
+    // let fetch = mon"-"+year
+    /*  console.log(date);
+    console.log("new Date :>>", new Date(date)); */
+    // console.log("item.date", new Date(item.date).toDateString());
+    return monthDatePicker === monthItem && yearDatePicker === yearItem 
+
+      /* new Date(item.date).setHours(0, 0, 0, 0) ===
+      new Date(dateX).toLocaleDateString("de", {
+        month: "long",
+        year: "numeric", */
   });
   return filteredList;
 };
+
+/* $("#booDatePicker").datepicker({
+  format: "mm-yyyy",
+  startView: "months",
+  minViewMode: "months",
+});
+ */
 
 /*  const filteredListByDate = filterByDate(item);
  console.log("filteredListByDate:", filteredListByDate);
@@ -119,7 +164,7 @@ const controller = async () => {
   console.log("list :>> ", list);
   createTable(list);
   eventListener(list);
-  filterByDate(list);
+  tableDate(list);
 
   // createSort(data)
 };
